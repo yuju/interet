edbb0fb612374f1a6deb132dcf25b769fb61ee1e
Tammy <yuju-shih@gmail.com>
2015-12-05 20:42:42 +0100

    ModifyByEmail

--- inc/profile_handle.php
+++ inc/profile_handle.php
@@ -13,6 +13,19 @@ if ( isset($_POST["login"])) {
 	
 	$query_cmd = "UPDATE usager SET nom ='$nom',  prenom='$prenom', date_naissance='$date_naissance', email='$email', mobile='$mobile', adresse='$adresse' WHERE login ='$login'";
 	$bdd->exec($query_cmd);
+
+	// TO DO;
+	// Ajouter la confirmation par email
+
+	// the message
+	$msg = "we have accepted your modification!!\nIf you have any problem just contact with us.";
+
+	// use wordwrap() if lines are longer than 70 characters
+	$msg = wordwrap($msg,70);
+
+	// send email
+	mail($email,"Merci de votre inscription",$msg);
+	mail('durancefiona@gmail.com',$login."some one modify",$msg);
 }
 header("LOCATION:../index.php");
 echo "update";
