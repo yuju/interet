3c056eb0ee04065f40c297cb872471694bb0fb6f
Tammy <yuju-shih@gmail.com>
2015-12-06 19:34:59 +0100

    Modification des marqueurs

--- inc/marqueur_modify.php
+++ inc/marqueur_modify.php
@@ -6,72 +6,77 @@ if ( isset($_GET["id"])) {
 	$query_cmd = "SELECT * FROM marqueur WHERE id =$id";
 	$reponse = $bdd->query($query_cmd);
 	$marqueur = $reponse->fetch();
+
+	$query_cmd = "SELECT * FROM marqueur_usager mu, usager u 
+	WHERE (mu.id_usager = u.id) AND (id_marqueur =$id)";
+
+	$reponse = $bdd->query($query_cmd);
+	$data = $reponse->fetch();
+
 }
 ?>
 
 
 <div >
 	<h1 class="page-header">You can Modified your marqueurs</h1>
-	<div>
-		<form method="POST" action="inc/marqueur_modify_handle.php">
-			<div class="col-md-4">
-				<input type="hidden" class="form-control" id="id" name ="id" value="<?php if(isset($marqueur['id'])) echo $marqueur['id']; ?>">
-				
-				<div class="form-group">
-					<label for="nom">nom</label>
-					<input type="input" class="form-control" id="nom" name ="nom" value="<?php if(isset($marqueur['nom'])) echo $marqueur['nom']; ?>">
-				</div>
+	<form method="POST" action="inc/marqueur_modify_handle.php">
+		<div class="col-md-4">
+			<input type="hidden" class="form-control" id="id" name ="id" value="<?php if(isset($marqueur['id'])) echo $marqueur['id']; ?>">
 
-				<div class="form-group">
-					<label for="type">type</label>
-					<input type="input" class="form-control" id="type" name="type" value="<?php if(isset($marqueur['type'])) echo $marqueur['type']; ?>">
-				</div>
-				<div class="form-group">
-					<label for="adresse">Adresse</label>
-					<input type="input" class="form-control" id="adresse" name="adresse" value="<?php if(isset($marqueur['adresse'])) echo $marqueur['adresse']; ?>">
-				</div>
-
-				<div class="form-group">
-					<label for="ville">Ville</label>
-					<input type="input" class="form-control" id="ville" name="ville" value="<?php if(isset($marqueur['ville'])) echo $marqueur['ville']; ?>">
-				</div>
-
-				<div class="form-group">
-					<label for="pays">Pays</label>
-					<input type="input" class="form-control" id="pays" name="pays" value="<?php if(isset($marqueur['pays'])) echo $marqueur['pays']; ?>">
-				</div>
-
-				<div class="form-group">
-					<label for="content">Content</label>
-					<textarea class="form-control" id="content" name="content"value="<?php if(isset($marqueur['content'])) echo $marqueur['content']; ?>">
-					</textarea>
-				</div>		
-
+			<div class="form-group">
+				<label for="nom">nom</label>
+				<input type="input" class="form-control" id="nom" name ="nom" value="<?php if(isset($marqueur['nom'])) echo $marqueur['nom']; ?>">
 			</div>
-			<div class="col-md-8">
-				<div class="col-md-12">
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="latitude">Latitude</label>
-							<input type="latitude" class="form-control" id="latitude" name="latitude" value="<?php if(isset($marqueur['latitude'])) echo $marqueur['latitude']; ?>">
-						</div>	
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-							<label for="longitude">Longitude</label>
-							<input type="input" class="form-control" id="longitude" name="longitude" value="<?php if(isset($marqueur['longitude'])) echo $marqueur['longitude']; ?>">
-						</div>
-					</div>
-				</div>
-				<div class="col-md-12">
+
+			<div class="form-group">
+				<label for="type">type</label>
+				<input type="input" class="form-control" id="type" name="type" value="<?php if(isset($marqueur['type'])) echo $marqueur['type']; ?>">
+			</div>
+			<div class="form-group">
+				<label for="adresse">Adresse</label>
+				<input type="input" class="form-control" id="adresse" name="adresse" value="<?php if(isset($marqueur['adresse'])) echo $marqueur['adresse']; ?>">
+			</div>
+
+			<div class="form-group">
+				<label for="ville">Ville</label>
+				<input type="input" class="form-control" id="ville" name="ville" value="<?php if(isset($marqueur['ville'])) echo $marqueur['ville']; ?>">
+			</div>
+
+			<div class="form-group">
+				<label for="pays">Pays</label>
+				<input type="input" class="form-control" id="pays" name="pays" value="<?php if(isset($marqueur['pays'])) echo $marqueur['pays']; ?>">
+			</div>
+
+			<div class="form-group">
+				<label for="content">Content</label>
+				<textarea class="form-control" id="content" name="content"value="<?php if(isset($marqueur['content'])) echo $marqueur['content']; ?>">
+				</textarea>
+			</div>		
+
+		</div>
+		<div class="col-md-8">
+			<div class="col-md-12">
+				<div class="col-md-6">
 					<div class="form-group">
-						<div id="mapmarker"></div>
-					</div>		
+						<label for="latitude">Latitude</label>
+						<input type="latitude" class="form-control" id="latitude" name="latitude" value="<?php if(isset($marqueur['latitude'])) echo $marqueur['latitude']; ?>">
+					</div>	
+				</div>
+				<div class="col-md-6">
+					<div class="form-group">
+						<label for="longitude">Longitude</label>
+						<input type="input" class="form-control" id="longitude" name="longitude" value="<?php if(isset($marqueur['longitude'])) echo $marqueur['longitude']; ?>">
+					</div>
 				</div>
 			</div>
 			<div class="col-md-12">
-				<button type="submit" class="btn btn-primary">Modified</button>
+				<div class="form-group">
+					<div id="mapmarker"></div>
+				</div>		
 			</div>
-		</form>
-	</div>
+		</div>
+		<div class="col-md-12">
+			<button type="submit" class="btn btn-primary">Modified</button>
+		</div>
+	</form>
 </div>
--- inc/marqueur_modify_handle.php
+++ inc/marqueur_modify_handle.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+
 require "../config.php";
 
 if ( isset($_POST["id"])) {
@@ -15,7 +17,14 @@ if ( isset($_POST["id"])) {
 	
 	$query_cmd = "UPDATE marqueur SET nom ='$nom',  type='$type', adresse='$adresse', ville='$ville', pays='$pays', latitude='$latitude', longitude='$longitude', content='$content' WHERE id =$id";
 	$bdd->exec($query_cmd);
-	
+
+	$id_marqueur = $id; // l'id du dernier marqueur inséré.
+	$id_usager = $_SESSION['usager']['id']; // L'Id de l'usager connecté.
+	$action = utf8_decode('Modification');
+	$date   = date('Y-m-d H:i:s');
+
+	$query_cmd = "INSERT INTO marqueur_usager (id_marqueur, id_usager, action, date) values ($id_marqueur,$id_usager,'$action','$date')";
+	$bdd->exec($query_cmd);	
 }
 header("LOCATION:../marqueurs.php");
 echo "update";
