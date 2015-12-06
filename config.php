1ac71b36eab11bc3836da555212a6f544b3eaa87
Tammy <yuju-shih@gmail.com>
2015-12-06 13:52:06 +0100

    Paramétrer la fonction de conexion base de données

--- config.php
+++ config.php
@@ -1,7 +1,14 @@
 <?php
+
+$servername = "localhost";
+$user = "root";
+$password = "";
+$dbname = "interet";
+
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=interet', 'root', '');
+	$bdd = new PDO("mysql:host=$servername;dbname=$dbname", $user, $password);
+	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (Exception $e)
 {
