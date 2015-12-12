425c0abde207e4b2a9ca85f4dcf9da48011492a0
Tammy <yuju-shih@gmail.com>
2015-12-12 21:49:56 +0100

    Modifier le aside

--- inc/aside.php
+++ inc/aside.php
@@ -1,28 +1,28 @@
 <div>
 	<h2>Filtres</h2>
-	<form method="POST" action="inc/usager_add_handle.php">
+	<form id="filter-form" action="inc/search.php">
+		
 		<div class="form-group">
-			<label for="nom">Nom</label>
+			<label for="nom">Nom de place</label>
 			<input type="input" class="form-control" id="nom" name ="nom">
 		</div>
 
 		<div class="form-group">
-			<label for="prenom">Type</label>
-			<input type="input" class="form-control" id="prenom" name="prenom">
-		</div>
-		<div class="form-group">
-			<label for="date_naissance">Date de naissance</label>
-			<input type="input" class="form-control" id="date_naissance" name="date_naissance">
+			<label for="type">Type</label>
+			<input type="input" class="form-control" id="type" name="type">
 		</div>
 
 		<div class="form-group">
-			<label for="login">Login</label>
-			<input type="input" class="form-control" id="login" name="login">
+			<label for="pays">Pays</label>
+			<input type="input" class="form-control" id="pays" name="pays">
 		</div>
+
 		<div class="form-group">
-			<label for="password">Mot de passe</label>
-			<input type="password" class="form-control" id="password" name="password">
+			<label for="ville">Ville</label>
+			<input type="input" class="form-control" id="ville" name="ville">
 		</div>
+
+		<button type="submit" class="btn btn-primary pull-right">Search</button>
 
 	</form>
 </div>
