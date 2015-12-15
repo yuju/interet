0c8f0deb475a78898f448f773fec65f026302007
Tammy <yuju-shih@gmail.com>
2015-12-15 21:11:01 +0100

    open page create

---
+++ open_page.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>Made by Yu-Ju SHIH</title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	<!-- Custom styles for this template -->
+	<link href="assets/css/dashboard.css" rel="stylesheet">
+	<link href="assets/css/style.css" rel="stylesheet">
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+		<script src="js/script.js"></script>
+</head>
+
+<body>
+<div id="right_header" class="col-md-12">
+		<marquee onMouseOver="this.stop()" onMouseOut="this.start()" height="50" behavior=" alternate" scrolldelay="4" scrollamount="3">
+		<h4><a href="mailto:yuju.shih@gmail.com" target="_top" style="color:#666699">Hello!! Welcome to my presonal blog, if you have any question, please mail to: yuju.shih@gmail.com</a></h4></marquee>
+
+		
+	</div>
+
+<div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+			<li data-target="#myCarousel" data-slide-to="1"></li>
+			<li data-target="#myCarousel" data-slide-to="2"></li>
+			<li data-target="#myCarousel" data-slide-to="3"></li>
+		</ol>
+
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<div class="item active">
+				<img src="img/cover_1.jpg" alt="Chania" height="500px" width="600px" >
+			</div>
+
+			<div class="item">
+				<img src="img/cover_2.jpg" alt="Chania" height="500px" width="600px" >
+			</div>
+
+			<div class="item">
+				<img src="img/cover_3.jpg" alt="Flower" height="500px" width="600px" >
+			</div>
+			
+
+		</div>
+
+		<!-- Left and right controls -->
+		<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+
+			</div>
+			
+</div>
+<div class="col-sm-7 col-sm-offset-5">
+			<a type="submit" class="btn btn-primary" href="index.php" >Enter my website</a>
+			</div>
+
+</body>
