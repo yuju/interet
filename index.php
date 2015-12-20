3f0ad31503278f9973438db4bd3eedd335d6bea3
Tammy <yuju-shih@gmail.com>
2015-12-20 13:17:29 +0100

    ajout fontowsome

--- index.php
+++ index.php
@@ -13,10 +13,10 @@
 
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-
 	<!-- Optional theme -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
-	
+	<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
 	<!-- Custom styles for this template -->
 	<link href="assets/css/dashboard.css" rel="stylesheet">
 	<link href="assets/css/style.css" rel="stylesheet">
