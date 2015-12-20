0638384c14ee4da109a935908a5990fe605f6980
Tammy <yuju-shih@gmail.com>
2015-12-20 19:55:18 +0100

    aboutme

---
+++ inc/aboutme.php
@@ -0,0 +1,38 @@
+
+        <div id="conteneur">
+                <header>
+                        <div id="photo"><img src="images/20150927.jpg" title="Dimitri DUBOIS" alt="Dimitri DUBOIS" width="74" height="110"/></div>
+                        <h1>Dimitri DUBOIS</h1>
+                        <p>Ingénieur CNRS<br />
+			Université de Montpellier - Faculté d'Economie<br />
+			<em>Laboratoire Montpelliérain d'Economie Théorique et Appliquée</em></p>
+                </header>
+
+                <div id="separation"></div>
+
+                <nav >
+			<ul >
+				<a href="index.php?page=enBref&amp;lang=fr"><li>En bref</li></a>
+				<a href="index.php?page=autres&amp;lang=fr"><li>Responsabilit&eacute;s et expertises</li></a>
+				<a href="index.php?page=recherche&amp;lang=fr"><li>Recherche</li></a>
+				<a href="index.php?page=developpement&amp;lang=fr"><li>Développements informatiques</li></a>
+				<a href="index.php?page=enseignement&amp;lang=fr"><li>Enseignements</li></a>
+				<a href="index.php?page=parcours&amp;lang=fr"><li>Parcours et diplômes</li></a>
+				<a href="index.php?page=formations&amp;lang=fr"><li>Formations professionnelles</li></a>
+				<a href="index.php?page=enBref&amp;lang=en"><li>English version</li></a>
+			</ul>
+		</nav>
+                <section>
+                        <article><p><strong>Ingénieur</strong><br />Docteur en Sciences Economiques<br />Responsable exécutif du Laboratoire d'Economie Expérimentale de Montpellier (LEEM)<br />Responsable du Diplôme Universitaire <em>Connaissances Fondamentales en Informatique et Logiciel Libre (CFI2L)</em> - (<i><a href="http://offre-formation-16.univ-montp1.fr/fr/offre-de-formation/diplome-universite-niv-form-bac-4-UE/droit-economie-gestion-DEG/diplome-d-universite-connaissances-fondamentales-en-informatique-et-logiciel-libre-program-fruai0341087xprhx4d3ymb.html" target="_blank">Informations et dossier de candidature</a></i>)</p>
+<p><em>Laboratoire Montpelli&eacute;rain d'Economie Th&eacute;orique et Appliqu&eacute;e (LAMETA)</em><br />
+Université de Montpellier, Faculté d'Economie<br />
+Site de Richter - Avenue Raymond Dugrand<br />
+CS 79606 - 34960 Montpellier Cedex 2<br>France</p>
+<p>Bureau 201a<br />
+Tel: 04-34-43-25-42<br />
+Mail: <a href="mailto:dimitri.dubois@lameta.univ-montp1.fr">dimitri.dubois@lameta.univ-montp1.fr</a></p>
+
+</article>
+                </section>
+        </div>
+
