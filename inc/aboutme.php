f239bdfdf8ac7b924fcdceafa0ba59d3ac985b10
Tammy <yuju-shih@gmail.com>
2015-12-20 22:11:18 +0100

    aboutme

--- inc/aboutme.php
+++ inc/aboutme.php
@@ -1,38 +1,188 @@
+<div class="col-md-12">
+	<div class="col-md-1">
+		<img src="assets/img/cvphoto2.jpg" title="Yu-Ju SHIH" alt="Yu-Ju SHIH" width="100"/>
+	</div>
+	<div class="col-md-11">
+	<blockquote class="blockquote">
+		<h1>Yu-Ju SHIH</h1>
+		<p>Ingénieur UM2<br />
+			Université de Montpellier II - Informatique pour les sciences<br/>
+			<em>Laboratoire Montpelliérain d'Informatique</em>
+		</p>
+		</blockquote>
+	</div>
+</div>
 
-        <div id="conteneur">
-                <header>
-                        <div id="photo"><img src="images/20150927.jpg" title="Dimitri DUBOIS" alt="Dimitri DUBOIS" width="74" height="110"/></div>
-                        <h1>Dimitri DUBOIS</h1>
-                        <p>Ingénieur CNRS<br />
-			Université de Montpellier - Faculté d'Economie<br />
-			<em>Laboratoire Montpelliérain d'Economie Théorique et Appliquée</em></p>
-                </header>
+<div class="col-md-12">
+<div class="col-md-12">
+	<p>
+<dl>
+  <dt><h3><a textcolor="red">Education</a></h3></dt>
+  <dd>
+  <table class="table table-condensed">
+    <thead>
+      <tr>
+        <th>Year</th>
+        <th>Diploma</th>
 
-                <div id="separation"></div>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>2001-2005</td>
+        <td>B.S., Chung Yuan Christian University (CYCU), Dept. of Chemical Engineering</td>
+      </tr>
+      <tr>
+        <td>2006-2008</td>
+        <td>M.S., Chung Yuan Christian University (CYCU), Dept. of Chemical Engineering</td>
+      </tr>
+      <tr>
+        <td>2010-2012</td>
+        <td>PhD., Universite Montpellier II, Sciences et Techniques du Languedoc, Science Chimiques</td>
+      </tr>
+	  <tr>
+        <td>2012-2013</td>
+        <td>Postdoctoral Fellow, R&D Center for Membrane Technology, Chung Yuan Christian University Department of Chemical Engineering</td>
+      </tr>
+      <tr>
+        <td>2015-Now</td>
+        <td>M.S., Universite Montpellier II, Sciences et Techniques du Languedoc, Informatique pour les sciences</td>
+      </tr>
+    </tbody>
+  </table></dd>
+</dl>
+</p>
+	
+<h3><a textcolor="red">Personal Skill</a></h3>
+<li><strong>Language skill</strong></li>
+<table class="table table-bordered">
+    <thead>
+      <tr>
+	  <th>Language</th>
+        <th>Understanding</th>
+        <th>Speaking</th>
+        <th>Writting</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+	  <td>Chinese</td>
+        <td>Mother Tongue</td>
+        <td>Mother Tongue</td>
+        <td>Mother Tongue</td>
+      </tr>
+      <tr>
+	  <td>English</td>
+        <td>Proficient user</td>
+        <td>Proficient user</td>
+        <td>Proficient user</td>
+      </tr>
+      <tr>
+	  <td>French</td>
+        <td>Basic user</td>
+        <td>Basic user</td>
+        <td>Basic user</td>
+      </tr>
+	</tbody>
+  </table>
+   <li><strong>Laboratory skill</strong></li>
+   <table class="table table-bordered">
+    <thead>
+      <tr>
+	  <th>Skills</th>
+        <th>Detail</th>
+       
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+	  <td>Material Design</td>
+        <td>Organic (co)polymer synthesis, membrane/film preparation, (Co)polymer design, coating technology on gold/metal/glass/membrane/culture dish surface via grafting from or grafting onto methods, structure of gene/drug delivery, blood filter design, hydrogel preparation, interfacial study of biomaterials.
+ 
+</td>
+        
+      </tr>
+      <tr>
+	  <td>Surface Functionalization</td>
+        <td>Methods: self assemble monolayer(SAM), ATRP and RAFT. 
+Surface: gold chip/glass/metal/membrane/culture dish.
+Type: biotin/market conjugation, enzyme/protein immobilization.
+</td>
+        
+      <tr>
+	  <td>Cell Culture</td>
+        <td>Cell culture room management, fluorescence staining, counting, cell viability test and MTT test, evaluation of interaction between cell and biomaterials</td>
+      </tr>
+	  <tr>
+	  <td>Microorganism</td>
+        <td>Culture Bacteria culture, Evaluation of interaction between bacteria and biomaterials.</tr>
+	  <tr>
+	  <td>Gene delivery</td>
+        <td>Gene transfection test</tr>
+	  <tr>
+	  <td>Assay tests</td>
+        <td>Enzyme Linked Immunosorbent assay (ELISA), ethidium bromide displacement assay, gel retardation assay</tr>
+	  
+	  <tr>
+	  <td>Blood compatibility test</td>
+        <td>Blood protein adsorption test (ELISA), platelet adhesion test, blood cell/platelet attachment test, blood coagulation time test and hemolysis test.</tr>
+	  
+	</tbody>
+  </table>
+   <li><strong>Instrumentations Skill</strong></li>
+   <table class="table table-bordered">
+    <thead>
+      <tr>
+	  <th>Skills</th>
+        <th>Detail</th>
+       
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+	  <td>Microscopy</td>
+        <td>Optical microscope, fluorescence microscope, confocal laser scanning microscopy(CLSM), scanning electron microscopy(SEM), atomic force microscopy(AFM)</td>
+        
+      </tr>
+	  <tr>
+	  <td>Spectroscopy</td>
+        <td>Ultraviolet-visible spectroscopy, spectraMax M5 (Absorbance, fluorescence and luminescence), dynamic light scattering (DLS), FT-IR and IR, NMR spectroscopy</td>
+		</tr>
+	  
+	  </tbody>
+  </table>
+   <li><strong>Computer Skill </strong></li>
+   <P>Microsoft Office, Origin Lab, Endnote, MATLAB , Chemical sketch and ChemDraw.</P>
+   
+   <li><strong>Artistic Skill </strong></li>
+   <P>Drawing, music and art design.</P>
+   </ul>
 
-                <nav >
-			<ul >
-				<a href="index.php?page=enBref&amp;lang=fr"><li>En bref</li></a>
-				<a href="index.php?page=autres&amp;lang=fr"><li>Responsabilit&eacute;s et expertises</li></a>
-				<a href="index.php?page=recherche&amp;lang=fr"><li>Recherche</li></a>
-				<a href="index.php?page=developpement&amp;lang=fr"><li>Développements informatiques</li></a>
-				<a href="index.php?page=enseignement&amp;lang=fr"><li>Enseignements</li></a>
-				<a href="index.php?page=parcours&amp;lang=fr"><li>Parcours et diplômes</li></a>
-				<a href="index.php?page=formations&amp;lang=fr"><li>Formations professionnelles</li></a>
-				<a href="index.php?page=enBref&amp;lang=en"><li>English version</li></a>
-			</ul>
-		</nav>
-                <section>
-                        <article><p><strong>Ingénieur</strong><br />Docteur en Sciences Economiques<br />Responsable exécutif du Laboratoire d'Economie Expérimentale de Montpellier (LEEM)<br />Responsable du Diplôme Universitaire <em>Connaissances Fondamentales en Informatique et Logiciel Libre (CFI2L)</em> - (<i><a href="http://offre-formation-16.univ-montp1.fr/fr/offre-de-formation/diplome-universite-niv-form-bac-4-UE/droit-economie-gestion-DEG/diplome-d-universite-connaissances-fondamentales-en-informatique-et-logiciel-libre-program-fruai0341087xprhx4d3ymb.html" target="_blank">Informations et dossier de candidature</a></i>)</p>
-<p><em>Laboratoire Montpelli&eacute;rain d'Economie Th&eacute;orique et Appliqu&eacute;e (LAMETA)</em><br />
-Université de Montpellier, Faculté d'Economie<br />
-Site de Richter - Avenue Raymond Dugrand<br />
-CS 79606 - 34960 Montpellier Cedex 2<br>France</p>
-<p>Bureau 201a<br />
-Tel: 04-34-43-25-42<br />
-Mail: <a href="mailto:dimitri.dubois@lameta.univ-montp1.fr">dimitri.dubois@lameta.univ-montp1.fr</a></p>
 
-</article>
-                </section>
-        </div>
+<br/><br/>
+	<p>
+		<address>
+		<strong>Informatique pour les sciences, UM2.</strong><br>
+			1355 Market Street, Suite 900<br>
+			Montpellier, 34090 Montpellier<br>
+			<abbr title="Phone">P:</abbr> (+33) 06 15 80 91 23
+		</address>
+
+		<address>
+			<strong>Yu-Ju SHIH</strong><br>
+			<a href="mailto:#">yuju.shih@gmail.com</a>
+		</address>
+	</p>
+</div>
+</div>
+
+
+<blockquote class="blockquote-reverse">
+  ...
+</blockquote>
+
+<dl>
+  <dt>...</dt>
+  <dd>...</dd>
+</dl>
 
