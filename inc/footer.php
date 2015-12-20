a7c05dfbf1dcf3e11b5050ef3b99fcaedda4eb4f
Tammy <yuju-shih@gmail.com>
2015-12-20 11:47:35 +0100

    Modification du footer

--- inc/footer.php
+++ inc/footer.php
@@ -3,3 +3,17 @@
 <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script> 
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <script src="assets/js/script.js"></script>
+
+<nav class="navbar navbar-default navbar-fixed-bottom">
+	<div class="container-fluid">
+		<div id="navbar" class="navbar-collapse collapse">
+			<ul class="nav navbar-nav navbar-right">
+				<li><a id="aboutme"href="#"><i id="social-fb" class="fa fa-info-circle fa-3x"></i></a></li>
+				<li><a href="https://www.facebook.com/yuju.hidjashih"><i id="social-fb" class="fa fa-facebook-square fa-3x social"></i></a></li>
+				<li><a href="https://twitter.com/yuju.shih"><i id="social-tw" class="fa fa-twitter-square fa-3x social"></i></a></li>
+				<li><a href="https://plus.google.com/+yuju.shih-page"><i id="social-gp" class="fa fa-google-plus-square fa-3x social"></i></a></li>
+				<li><a id="contact" href="#"><i id="social-em" class="fa fa-envelope-square fa-3x social"></i></a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
