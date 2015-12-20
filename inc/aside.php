eb58ec5432c928a871a6164a2decc1b10119c92d
Tammy <yuju-shih@gmail.com>
2015-12-20 22:10:31 +0100

    aside

--- inc/aside.php
+++ inc/aside.php
@@ -23,3 +23,32 @@
 		?>
 	</tbody>
 </table>
+
+<h4>Les merqueurs add</h4>
+<table class="table table-striped">
+	<thead>
+		<tr>
+			<th>Nom</th>
+			<th>Type</th>
+			<th>Pays</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php 
+
+		$query_cmd = "SELECT * FROM marqueur ORDER BY id DESC LIMIT 5";
+		$reponse = $bdd->query($query_cmd);
+		while ($donnees = $reponse->fetch())
+		{
+			?>
+			<tr>
+				<td><?php echo $donnees["nom"]; ?> </td>
+				<td><?php echo $donnees["type"]; ?> </td>
+				<td><?php echo $donnees["pays"]; ?> </td>
+			</tr>
+			</tr>
+			<?php
+		}
+		?>
+	</tbody>
+</table>
