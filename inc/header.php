09fbea334e07520ed754051d8c0b6a72723b62d9
Tammy <yuju-shih@gmail.com>
2015-12-21 12:56:33 +0100

    changer header title

--- inc/header.php
+++ inc/header.php
@@ -7,7 +7,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="index.php">Point d'Interet Touristique à Taïwan</a>
+			<a class="navbar-brand" href="index.php">Point d'Interet Touristique</a>
 		</div>
 
 		<div id="navbar" class="navbar-collapse collapse">
